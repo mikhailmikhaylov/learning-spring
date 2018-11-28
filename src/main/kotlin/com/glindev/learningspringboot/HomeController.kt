83525e1afb55c13e74061af7f21e1b6bcdde741d
Mykhailo Mykhailov <mmykhailov@atlassian.com>
2018-11-29 00:25:19 +0200

    Add actuator plugin and enable metrics.

--- src/main/kotlin/com/glindev/learningspringboot/HomeController.kt
+++ src/main/kotlin/com/glindev/learningspringboot/HomeController.kt
@@ -4,11 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@RestController class HomeController {
+@RestController
+class HomeController {
 
-    @GetMapping fun greeting(
-            @RequestParam(required = false, defaultValue = "") name: String
-    ): String {
+    @GetMapping
+    fun greeting(@RequestParam(required = false, defaultValue = "") name: String): String {
         return if (name == "") "Hey!" else "Hey, $name!"
     }
 }
