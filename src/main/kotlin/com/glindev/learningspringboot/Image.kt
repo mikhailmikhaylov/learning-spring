125e7b010cba948ec94b332933ed47aca16353e5
Mykhailo Mykhailov <nutscracker53@gmail.com>
2018-12-21 00:25:32 +0200

    Remove lombok dependency

--- src/main/kotlin/com/glindev/learningspringboot/Image.kt
+++ src/main/kotlin/com/glindev/learningspringboot/Image.kt
@@ -1,12 +1,10 @@
 package com.glindev.learningspringboot
 
-import lombok.Data
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
-@Data
 @Document
-class Image(
+data class Image(
         @Id val id: String,
         val name: String
 )
